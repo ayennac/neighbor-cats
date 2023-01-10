75e00c8d2e04277b12a129da9e9452a3d89b0f50
ayennac <ayennac@gmail.com>
2023-01-10 12:34:29 -0800

    moved files around and added react-router-dom

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import {Link} from 'react-router-dom'
+//this link component handles the routing locally in the browser instead of requesting from the server
+
+const Navbar = () =>{
+    return(
+        <header>
+        <div className = "container">
+            <Link to="/">
+            <h1>Neighbor Cats</h1>
+            </Link>
+        </div>
+        </header>
+    )
+}
+
+export default Navbar
