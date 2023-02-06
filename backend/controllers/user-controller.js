3ac91449bcb60b3494126bc1b99f472cd1024c23
ayennac <ayennac@gmail.com>
2023-02-06 11:16:40 -0800

    added json web token and token procedures

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,4 +1,9 @@
 const User = require('../models/user-model')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) =>{
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+}
 
 
 //login user
@@ -12,7 +17,12 @@ const signupUser = async(req, res) =>{
     const {fname, lname, email, password} = req.body
     try{
         const user = await User.signup(fname, lname, email, password)
-        res.status(200).json({user})
+
+        //create Token
+
+        const token = createToken(user._id)
+
+        res.status(200).json({user, token})
     } catch(error){
         res.status(400).json({error: error.message})
     }
