08ffb9e91a6131af604b930f3698bceddd2e1356
ayennac <ayennac@gmail.com>
2023-02-27 13:36:51 -0800

    protected routes with AuthContext on BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 
 //npm installed react-router-dom so that we can style individual pages
-import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Navigate} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import { useAuthContext } from './hooks/useAuthContext'
 
 //pages 
@@ -14,24 +14,27 @@ import Posts from './pages/Posts'
 import RootLayout from './layouts/RootLayout'
 
 
-const router = createBrowserRouter(
 
-  createRoutesFromElements(
-    <Route path = "/" element = {<RootLayout/>}>
-    <Route index element={<Home/>}/>
-    <Route path = "about" element = {<About/>}/>
-    <Route path = "login" element = {<Login/>}/>
-    <Route path = "signup" element = {<Signup/>}/>
-    <Route path = "posts" element = {<Posts/>}/>
-    <Route path = "*" element ={<p>Oops! There's nothing here!</p>}/>
-
-    </Route>
-  )
-)
 
 const App = () => {
+  const {user} = useAuthContext()
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path = "/" element = {<RootLayout/>}>
+      <Route index element={user ? < Home/> : <Navigate to="/login"/>}/>
+      <Route path = "about" element = {<About/>}/>
+      <Route path = "login"  element ={!user ? <Login/>: <Navigate to="/"/>}/>
+      <Route path = "signup" element ={!user ? <Signup/>: <Navigate to="/"/>}/>
+      <Route path = "posts" element = {<Posts/>}/>
+      <Route path = "*" element ={<p>Oops! There's nothing here!</p>}/>
+      </Route>
+    )
+  )
+
+
    return (
-      <RouterProvider router = {router} >
+      <RouterProvider router = {router}>
 
       </RouterProvider>
   );
